aaf7e12a68e22c7bca86590a196f2fb08dc6d375
Krutarth1993 <kpj3111993@gmail.com>
2022-05-07 16:47:02 -0400

    Protect Routes and Authentication
    Created new file authMiddleware.js in middleware folder.
    And in userRoutes.js file gave protect

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const { append } = require("express/lib/response");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/userControllers");
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require("../controllers/userControllers");
+
+const { protect } = require("../middleware/authMiddleware");
 
 router.post("/", registerUser);
-
 router.post("/login", loginUser);
+router.get("/me", protect, getMe);
 
 module.exports = router;
