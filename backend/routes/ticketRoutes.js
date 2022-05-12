f719020e75d12b6d6b83ccba033eb72bac4ad797
Krutarth1993 <kpj3111993@gmail.com>
2022-05-12 08:37:03 -0400

    Single Ticket, Update and Delete (BackEnd)

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const { getTickets, createTicket } = require("../controllers/ticketController");
+const {
+  getTickets,
+  getTicket,
+  createTicket,
+  deleteTicket,
+  updateTicket,
+} = require("../controllers/ticketController");
 
 const { protect } = require("../middleware/authMiddleware");
 
 // In order to get your tickets you have to be authenticated
 router.route("/").get(protect, getTickets).post(protect, createTicket);
+
+router
+  .route("/:id")
+  .get(protect, getTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket);
 
 module.exports = router;
